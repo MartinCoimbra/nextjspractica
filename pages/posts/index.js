8565f9f5b110b1679fc7ccee35aea10c08fe77e8
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-07-19 13:35:03 -0700

    featch api post

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,19 @@
+import React from "react";
 import Layout from "../../components/layout";
 import Title from "../../components/title";
 
 export default function Posts(){
+  const [posts, setPosts] = React.useState([])
+  /* https://jsonplaceholder.typicode.com/posts */
+  React.useEffect(()=>{
+    const fetchPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const newPost = await res.json();
+      console.log('newPost :', newPost);
+    }
+    fetchPosts();
+  },[])
+  
     return(
       <Layout>
         <Title> POSTEAR </Title>
