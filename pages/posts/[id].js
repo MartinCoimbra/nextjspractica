6042bbb18dc04526bdf98e177f1771ccc29eac49
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-07-20 12:48:25 -0700

    styles y rutass dinamicas con fetch

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,12 +2,58 @@ import { useRouter } from 'next/router'
 import Layout from '../../components/layout'
 import Title from "../../components/title";
 
-export default function User(){
-    const router = useRouter()
+export default function User({ post }){
+    /* const router = useRouter() */
+    {/* <p>POST ID: {router.query.id} </p> */}
     return(
       <Layout>
         <Title>Details POST</Title>
-        <p>POST ID: {router.query.id} </p>
+        <div className="card">
+            <h2>{post.id + " - " + post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+        <style jsx>
+        {` 
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
+
+    
       </Layout>
     )
   }
+
+export async function getServerSideProps({ params }){
+ /* context.params.id */
+ const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+ const post = await res.json()
+ return {
+   props: {
+     post
+   }
+ }
+}
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -66,6 +66,8 @@ export default function Posts({ posts }) {
           }
         `}
       </style>
+
+    
     </Layout>
   );
 }
