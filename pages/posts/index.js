6cc31ffe7f49df3f127e619adec775fdc5ba0b6b
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-07-20 11:02:48 -0700

    map

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,23 +1,35 @@
+import Link from "next/link";
 import React from "react";
 import Layout from "../../components/layout";
 import Title from "../../components/title";
 
-export default function Posts(){
-  const [posts, setPosts] = React.useState([])
+export default function Posts() {
+  const [posts, setPosts] = React.useState([]);
   /* https://jsonplaceholder.typicode.com/posts */
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       const newPost = await res.json();
-      console.log('newPost :', newPost);
-    }
+      setPosts(newPost);
+    };
     fetchPosts();
-  },[])
-  
-    return(
-      <Layout>
-        <Title> POSTEAR </Title>
-        <p>POSTPOSTPOSTPOSTPOSTPOST</p>
-      </Layout>
-    )
-  }
+  }, []);
+
+  return (
+    <Layout>
+      <Title> POSTEAR </Title>
+      <div>
+        {posts.map((post) => {
+          return (
+            <Link key={post} href={`/posts/[id]`} as={`/posts/${post.id}`}>
+              <a>
+                <h3> {post.title} </h3>
+                <a> {post.body} </a>
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+}
